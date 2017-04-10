a99c78544da71c40aa341929d677dca4028343b1
Simon Wu <siw@microsoft.com>
2017-04-10 17:26:53 +0800

    add MergeDepot

---
+++ MergeDepot.ps1
@@ -0,0 +1,48 @@
+Param(
+  [string]$DhsConnectionConfigs,
+  [string]$DepotToMerge,
+  [string]$Domain,
+  [string]$BaseUrl,
+  [string]$BaseFolder
+)
+
+Add-type -AssemblyName "System.IO.Compression.FileSystem"
+$mergeDepotToolContainerUrl = "https://siwtest.blob.core.windows.net/mergedepot"
+
+if(!(Test-Path ".optemp"))
+{
+    New-Item ".optemp" -ItemType Directory
+}
+
+$currentFolder = Get-Location
+$MergeDepotToolSource = "$mergeDepotToolContainerUrl/MergeDepotTool_v2.zip"
+$MergeDepotToolDestination = "$currentFolder\.optemp\MergeDepotTool.zip"
+
+echo 'Start Download!'
+Invoke-WebRequest -Uri $MergeDepotToolSource -OutFile $MergeDepotToolDestination
+echo 'Download Success!'
+
+$MergeDepotToolUnzipFolder = "$currentFolder\.optemp\MergeDepotTool"
+if((Test-Path "$MergeDepotToolUnzipFolder"))
+{
+    Remove-Item $MergeDepotToolUnzipFolder -Force -Recurse
+}
+
+[System.IO.Compression.ZipFile]::ExtractToDirectory($MergeDepotToolDestination, $MergeDepotToolUnzipFolder)
+echo 'Extract Success!' 
+$MergeDepotTool = "$MergeDepotToolUnzipFolder\MergeDepot.exe"
+
+git checkout master 2>&1 | Write-Host
+git status
+
+echo "Start to call merge depot tool"
+&"$MergeDepotTool" "$currentFolder\mergedepot" "-c" "$DhsConnectionConfigs" "-m" "$DepotToMerge" "-u" "$BaseUrl" "-f" "$BaseFolder" "-d" "$Domain"
+echo "Finish calling merge depot tool"
+
+echo "Start to push to git repository"
+git config --global core.safecrlf false
+git add * -A -v
+echo "Finish Git add."
+git commit -m "update" 2>&1 | Write-Host
+git push origin master 2>&1 | Write-Host
+echo "Finish pushing to git repository"
