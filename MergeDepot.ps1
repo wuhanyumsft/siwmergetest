ee47f5ca01a06a35261dbab236feee807322ae90
Simon Wu <siw@microsoft.com>
2017-04-11 13:50:59 +0800

    update script

--- MergeDepot.ps1
+++ MergeDepot.ps1
@@ -33,17 +33,6 @@ if((Test-Path "$MergeDepotToolUnzipFolder"))
 echo 'Extract Success!' 
 $MergeDepotTool = "$MergeDepotToolUnzipFolder\MergeDepot.exe"
 
-git checkout master 2>&1 | Write-Host
-git status
-
 echo "Start to call merge depot tool"
 &"$MergeDepotTool" "$currentFolder\mergedepot" "-c" "$DhsConnectionConfigs" "-m" "$DepotToMerge" "-u" "$BaseUrl" "-f" "$BaseFolder" "-d" "$Domain" "-t" "$TargetDepot"
 echo "Finish calling merge depot tool"
-
-echo "Start to push to git repository"
-git config --global core.safecrlf false
-git add * -A -v
-echo "Finish Git add."
-git commit -m "update" 2>&1 | Write-Host
-git push origin master 2>&1 | Write-Host
-echo "Finish pushing to git repository"
